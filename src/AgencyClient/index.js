18da66c5e1e80f42d648091d1a9832bf4483fbae
Neil Nienaber <neil.nienaber@gmail.com>
2021-08-12 10:44:34 +0200

    adding basic event sequencer
    This will allow us to add the new chrono_ids to the new events, which we
    can then apply a unique index to. This will act as our semaphore

--- src/AgencyClient/index.js
+++ src/AgencyClient/index.js
@@ -6,22 +6,29 @@ const events = require('./eventstore.json')
 const projections = {
   'ClientLinkCreated': (aggregate, event) => {
     console.log('ClientLinkCreated');
-    return event.data;
+    return {...event.data, last_chrono_id: event.chrono_id};
   },
   'ClientUnLinked': (aggregate, event) => {
     console.log('ClientUnLinked');
     aggregate.linked = false
-    return aggregate;
+    return {...aggregate, last_chrono_id: event.chrono_id};
   },
   'ClientConsultantAdded': (aggregate, event) => {
-    console.log('ClientUnLinked');
+    console.log('ClientConsultantAdded');
     let consultant = {};
     consultant.consultant_type = event.data.consultant_type;
     consultant.consultant_id = event.data.consultant_id;
     (aggregate.consultants) ?
       aggregate.consultants.push(consultant) :
       aggregate.consultants = [consultant];
-    return aggregate;
+    return {...aggregate, last_chrono_id: event.chrono_id};
+  },
+  'ClientConsultantRemoved': (aggregate, event) => {
+    console.log('ClientConsultantRemoved');
+    aggregate.consultants = _.differenceWith(aggregate.consultants, [event.data], function(value, other) {
+      return ((value.consultant_type == other.consultant_type) && (value.consultant_id == other.consultant_id))
+    });
+    return {...aggregate, last_chrono_id: event.chrono_id};
   }
 }
 
