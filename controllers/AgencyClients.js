0b84a909407e63b2696b2efdb51de584a78bf346
Neil Nienaber <neil.nienaber@gmail.com>
2021-08-30 17:45:48 +0200

    Adding the Generic Repository idea
    implemented a couple for testing and review purposes

--- controllers/AgencyClients.js
+++ controllers/AgencyClients.js
@@ -1,87 +1,12 @@
 'use strict';
+const {GenericRepository} = require('../src/GenericRepository');
 const {AgencyClientRepository} = require('../src/AgencyClient/AgencyClientRepository');
 const {AgencyClientCommandHandler} = require('../src/AgencyClient/AgencyClientCommandHandler');
 const {get} = require('lodash');
 const _ = require('lodash');
-const {EventStore} = require('../models');
+const {EventStore, AgencyClients} = require('../models');
 const {QueryHelper} = require('a24-node-query-utils');
-const {AgencyClientsService} = require('../services/AgencyClientsService');
 const {PaginationHelper} = require('../helpers/PaginationHelper');
-
-/**
- * Add Agency Client Consultant
- *
- * @param {ClientRequest} req - The http request object
- * @param {IncomingMessage} res - The http response object
- * @param {function} next - The callback used to pass control to the next action/middleware
- */
-module.exports.addAgencyClientConsultant = async (req, res, next) => {
-  const payload = get(req, 'swagger.params.assign_client_consultant_payload.value', {});
-  const agency_id = get(req, 'swagger.params.agency_id.value', '');
-  const client_id = get(req, 'swagger.params.client_id.value', '');
-  const command_type = get(req, 'swagger.operation.x-octophant-event', '');
-
-  let repository = new AgencyClientRepository(EventStore);
-  let handler = new AgencyClientCommandHandler(repository);
-
-  // Decide how auth / audit data gets from here to the event in the event store.
-  let command = {
-    type: command_type,
-    data: payload
-  };
-
-  try {
-    // Passing in the agency and client ids here feels strange
-    await handler.apply(agency_id, client_id, command);
-    // This needs to be centralised and done better
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({status: 'completed'}));
-  } catch (err) {
-    // This needs to be centralised and done better
-    console.log('ERR THERE WAS', err);
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({message: err.message}));
-  }
-};
-
-/**
- * Remove Agency Client Consultant
- *
- * @param {ClientRequest} req - The http request object
- * @param {IncomingMessage} res - The http response object
- * @param {function} next - The callback used to pass control to the next action/middleware
- */
-module.exports.removeAgencyClientConsultant = async (req, res, next) => {
-  const agency_id = get(req, 'swagger.params.agency_id.value', '');
-  const client_id = get(req, 'swagger.params.client_id.value', '');
-  const consultant_id = get(req, 'swagger.params.consultant_id.value', '');
-  const command_type = get(req, 'swagger.operation.x-octophant-event', '');
-
-  let repository = new AgencyClientRepository(EventStore);
-  let handler = new AgencyClientCommandHandler(repository);
-
-  // Decide how auth / audit data gets from here to the event in the event store.
-  let command = {
-    type: command_type,
-    data: {_id: consultant_id}
-  };
-
-  try {
-    await handler.apply(agency_id, client_id, command);
-    // This needs to be centralised and done better
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({status: 'completed'}));
-  } catch (err) {
-    // This needs to be centralised and done better
-    console.log('ERR THERE WAS', err);
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({message: err.message}));
-  }
-};
 
 /**
  * Gets a single agency client
@@ -130,9 +55,8 @@ module.exports.listAgencyClients = async (req, res, next) => {
 
   query.agency_id = get(req, 'swagger.params.agency_id.value', '');
 
-  const service = new AgencyClientsService(logger);
+  const service = new GenericRepository(logger, AgencyClients);
   try {
-
     const {count, data} = await service.listResources(query, limit, skip, sortBy);
     const statusCode = _.isEmpty(data) ? 204 : 200;
     await PaginationHelper.setPaginationHeaders(req, res, count);
---
+++ src/GenericRepository.js
@@ -0,0 +1,90 @@
+'use strict';
+const {RuntimeError} = require('a24-node-error-utils');
+
+/**
+ * AgencyClientsService
+ *  Most likely meant to be part of Repository / Aggregate code.
+ *  Based on the new implementation details the service layer no longer exists.
+ */
+class GenericRepository {
+  constructor(logger, collection) {
+    this.logger = logger;
+    this._store = collection;
+  }
+
+  /**
+   * Retrieves a list of all records for the given params
+   *
+   * @param {Object} query - The query object to retrieve list of tags
+   * @param {Number} limit - The item per page value
+   * @param {Number} skip - The skip value
+   * @param {Object} sortBy - The sortBy parameters
+   *
+   * @return Promise<Object>
+   */
+  async listResources(query, limit, skip, sortBy) {
+    try {
+      const [count, data] = await Promise.all([
+        _getCount(this._store, query),
+        _getListing(this._store, query, skip, limit, sortBy)
+      ]);
+      return {
+        count,
+        data
+      };
+    } catch (error) {
+      this.logger.error('The GET list call for tags failed', error);
+      throw error;
+    }
+  }
+}
+
+/**
+ * Gets the count of records that matches a given query
+ *
+ * @param {Object} query - The query to match
+ *
+ * @private
+ *
+ * @return Promise<Number>
+ */
+async function _getCount(store, query) {
+  try {
+    return await store.countDocuments(query);
+  } catch (dbError) {
+    throw new RuntimeError(
+      `An error occurred while counting records for ${store.modelName}`,
+      dbError
+    );
+  }
+}
+
+/**
+ * Get the list of records that matches the given query
+ *
+ * @param {Object} query - The query to match
+ * @param {Number} skip - The skip value
+ * @param {Number} limit - The item per page value
+ * @param {Object} sortBy - The sortBy parameters
+ *
+ * @private
+ *
+ * @return Promise<Array<Object>>
+ */
+async function _getListing(store, query, skip, limit, sortBy) {
+  try {
+    const list = await store.find(query)
+      .skip(skip)
+      .limit(limit)
+      .sort(sortBy)
+      .lean()
+      .exec();
+    return list;
+  } catch (dbError) {
+    throw new RuntimeError(`An error occurred while listing the records for ${store.modelName}`, {dbError});
+  }
+}
+
+module.exports = {
+  GenericRepository
+};
