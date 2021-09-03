10112d7d834bfa312dfa87d3ef21a047f8c4e5ab
Mehran Khajavi <sinoohe.mkh@gmail.com>
2021-09-03 19:04:13 +0430

    EOD: convert Agency codes to typescript #10

--- src-ts/Events.ts
+++ src-ts/Events.ts
@@ -1,42 +1,46 @@
-import _ from 'lodash';
-interface KeyValue {
-  [key: string]: String;
+
+export enum Events {
+  AGENCY_CONSULTANT_ROLE_ADDED = 'AgencyConsultantRoleAdded',
+  AGENCY_CONSULTANT_ROLE_ENABLED = 'AgencyConsultantRoleEnabled',
+  AGENCY_CONSULTANT_ROLE_DISABLED = 'AgencyConsultantRoleDisabled',
+  AGENCY_CONSULTANT_ROLE_DETAILS_UPDATED = 'The Agency Consultant Role has been updated',
+  AGENCY_CLIENT_CONSULTANT_ASSIGNED = 'AgencyClientConsultantAssigned',
+  AGENCY_CLIENT_CONSULTANT_UNASSIGNED = 'AgencyClientConsultantUnassigned',
+  AGENCY_CLIENT_LINKED = 'AgencyClientLinked',
+  AGENCY_CLIENT_UNLINKED = 'AgencyClientUnLinked'
 }
+
 const events = {
-  AGENCY_CONSULTANT_ROLE_ADDED: {
+  [Events.AGENCY_CONSULTANT_ROLE_ADDED]: {
     name: 'AgencyConsultantRoleAdded',
     description: 'The Agency Consultant Role has been created'
   },
-  AGENCY_CONSULTANT_ROLE_ENABLED: {
+  [Events.AGENCY_CONSULTANT_ROLE_ENABLED]: {
     name: 'AgencyConsultantRoleEnabled',
     description: 'The Agency Consultant Role has been enabled'
   },
-  AGENCY_CONSULTANT_ROLE_DISABLED: {
+  [Events.AGENCY_CONSULTANT_ROLE_DISABLED]: {
     name: 'AgencyConsultantRoleDisabled',
     description: 'The Agency Consultant Role has been disabled'
   },
-  AGENCY_CONSULTANT_ROLE_DETAILS_UPDATED: {
+  [Events.AGENCY_CONSULTANT_ROLE_DETAILS_UPDATED]: {
     name: 'AgencyConsultantRoleDetailsUpdated',
     description: 'The Agency Consultant Role has been updated'
   },
-  AGENCY_CLIENT_CONSULTANT_ASSIGNED: {
+  [Events.AGENCY_CLIENT_CONSULTANT_ASSIGNED]: {
     name: 'AgencyClientConsultantAssigned',
     description: 'The Agency Client Consultant has been assigned'
   },
-  AGENCY_CLIENT_CONSULTANT_UNASSIGNED: {
+  [Events.AGENCY_CLIENT_CONSULTANT_UNASSIGNED]: {
     name: 'AgencyClientConsultantUnassigned',
     description: 'The Agency Client Consultant has been unassigned'
   },
-  AGENCY_CLIENT_LINKED: {
+  [Events.AGENCY_CLIENT_LINKED]: {
     name: 'AgencyClientLinked',
     description: 'The Agency Client was linked'
   },
-  AGENCY_CLIENT_UNLINKED: {
+  [Events.AGENCY_CLIENT_UNLINKED]: {
     name: 'AgencyClientUnLinked',
     description: 'The Agency Client was unlinked, does not indicate a deletion'
   }
 };
-export const Events : KeyValue = _.reduce(events, (result: KeyValue, value, key) => {
-  result[key] = value.name;
-  return result;
-}, {});
