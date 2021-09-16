ee590707c571ea00bd646a48570cbe4ea5bb2d2a
Mehran Khajavi <sinoohe.mkh@gmail.com>
2021-09-16 18:24:13 +0430

    testing

--- tests/dropdatabase.ts
+++ tests/dropdatabase.ts
@@ -1,20 +1,19 @@
-// import config from 'config';
-// import mongoose from 'mongoose';
-// const env = process.env.NODE_ENV;
-// const {database_host: databaseHost, options} = config.get('mongo');
-// (async () => {
-//   try {
-//     if (env === 'testing') {
-//       await mongoose.connect(databaseHost, options);
-//       await mongoose.connection.db.dropDatabase();
-//       await mongoose.connection.close();
-//     } else {
-//       console.log('Not dropping database env: ', env);
-//     }
-//     process.exit(0);
-//   } catch (error) {
-//     console.log('Error in drop database', error);
-//     process.exit(1);
-//   }
-// })();
-console.log('WE DID NOTHING');
+import config from 'config';
+import mongoose from 'mongoose';
+const env = process.env.NODE_ENV;
+const {database_host: databaseHost, options} = config.get('mongo');
+(async () => {
+  try {
+    if (env === 'testing') {
+      await mongoose.connect(databaseHost, options);
+      await mongoose.connection.db.dropDatabase();
+      await mongoose.connection.close();
+    } else {
+      console.log('Not dropping database env: ', env);
+    }
+    process.exit(0);
+  } catch (error) {
+    console.log('Error in drop database', error);
+    process.exit(1);
+  }
+})();
