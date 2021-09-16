582103b42017ff8b3442910d1656069d697fafcc
Mehran Khajavi <sinoohe.mkh@gmail.com>
2021-09-16 18:57:13 +0430

    testing

---
+++ testfile-drop-database.js
@@ -0,0 +1,20 @@
+'use strict';
+const config = require('config');
+const mongoose = require('mongoose');
+const env = process.env.NODE_ENV;
+
+(async () => {
+  try {
+    if (env === 'testing') {
+      await mongoose.connect(config.mongo.database_host, config.mongo.options);
+      await mongoose.connection.db.dropDatabase(mongoose.connection.db);
+      await mongoose.connection.close();
+    } else {
+      console.log('Not dropping database env: ', env);
+    }
+    process.exit(0);
+  } catch (error) {
+    console.log('Error in drop database', error);
+    process.exit(1);
+  }
+})();
