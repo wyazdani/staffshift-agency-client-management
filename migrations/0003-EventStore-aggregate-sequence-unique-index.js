4839254c7b0c2d1e686f8a5eb5f18c4f18c30818
Boutar Badii <badii.boutar@a24group.com>
2021-09-22 15:58:44 +0100

    reverted migration changes

--- migrations/0003-EventStore-aggregate-sequence-unique-index.js
+++ migrations/0003-EventStore-aggregate-sequence-unique-index.js
@@ -1,35 +1,37 @@
 'use strict';
 const {ensureIndexExists, ensureIndexRemoved} = require('../tools/IndexExists');
 
-const indexDetails = {
-  aggregate_id: 1,
-  sequence_id: 1
+let indexDetails = {
+  'aggregate_id': 1,
+  'sequence_id': 1
 };
-
-const indexOptions = {
+let indexOptions = {
   background: true,
   unique: true
 };
-
 module.exports = {
   id: '0003-EventStore-aggregate-sequence-unique-index',
   up: (db, cb) => {
-    ensureIndexExists(db.collection('EventStore'), indexDetails, indexOptions)
-      .then(() => {
-        cb();
-      })
-      .catch((err) => {
-        cb(err);
-      });
+    ensureIndexExists(
+      db.collection('EventStore'),
+      indexDetails,
+      indexOptions
+    ).then(() => {
+      cb();
+    }).catch((err) => {
+      cb(err);
+    });
   },
 
   down: (db, cb) => {
-    ensureIndexRemoved(db.collection('EventStore'), indexDetails, indexOptions)
-      .then(() => {
-        cb();
-      })
-      .catch((err) => {
-        cb(err);
-      });
+    ensureIndexRemoved(
+      db.collection('EventStore'),
+      indexDetails,
+      indexOptions
+    ).then(() => {
+      cb();
+    }).catch((err) => {
+      cb(err);
+    });
   }
 };
