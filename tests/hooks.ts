4d64cc0b2c5eb15230191c697b1b2a611ee89f13
Eman <emmanuel.manyike@a24group.com>
2021-10-04 11:19:23 +0200

    Increase hook level timeout

--- tests/hooks.ts
+++ tests/hooks.ts
@@ -1,7 +1,9 @@
 import sinon from 'sinon';
 import {promise} from '../src/http_server';
 export const mochaHooks = {
-  before: (done: () => void) => {
+  before: function before(done: () => void) {
+    // TODO see if this helps with random failing test cases
+    this.timeout(35000);
     // eslint-disable-next-line no-console
     console.log('Waiting for http server to run');
     promise.then(done);
