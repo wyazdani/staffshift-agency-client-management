efe529fdf4cb74e3eb4288bd508f1e269ca346bd
Eman <emmanuel.manyike@a24group.com>
2021-10-04 11:32:04 +0200

    revert

--- tests/hooks.ts
+++ tests/hooks.ts
@@ -1,9 +1,7 @@
 import sinon from 'sinon';
 import {promise} from '../src/http_server';
 export const mochaHooks = {
-  before: function before(done: () => void) {
-    // TODO see if this helps with random failing test cases
-    this.timeout(35000);
+  before: (done: () => void) => {
     // eslint-disable-next-line no-console
     console.log('Waiting for http server to run');
     promise.then(done);
