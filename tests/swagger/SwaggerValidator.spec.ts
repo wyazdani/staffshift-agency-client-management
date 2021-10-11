543c5550189cad31ccca5b2841b026f3101fcff2
Mehran Khajavi <sinoohe.mkh@gmail.com>
2021-10-11 12:44:40 +0330

    Additional properties check main implementation #53

---
+++ tests/swagger/SwaggerValidator.spec.ts
@@ -0,0 +1,26 @@
+import * as path from 'path';
+import SwaggerParser from '@apidevtools/swagger-parser';
+import * as _ from 'lodash';
+import flat from 'flat';
+const swaggerFile = path.resolve(__dirname, '..', '..', 'api', 'swagger.yaml');
+
+describe('SwaggerValidator', () => {
+  it('Test all objects schemas should have additionalProperties set', async () => {
+    const api = await SwaggerParser.validate(swaggerFile);
+    const apiObj: any = flat(api.paths);
+
+    for (const key in apiObj) {
+      const typeKey = '.schema.type';
+
+      if (_.endsWith(key, typeKey)) {
+        if (apiObj[key] === 'object') {
+          const additionalKey = key.substring(0, key.length - typeKey.length) + '.schema.additionalProperties';
+
+          if (!_.has(apiObj, additionalKey)) {
+            throw new Error(`additionalProperties not set in ${additionalKey}`);
+          }
+        }
+      }
+    }
+  });
+});
