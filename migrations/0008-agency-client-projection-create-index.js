75d6dfa5dfcff0bd66923d12db20cf4e66fb1b86
Mehran Khajavi <sinoohe.mkh@gmail.com>
2021-10-27 17:45:16 +0330

    hotfix on migration #80

--- migrations/0008-agency-client-projection-create-index.js
+++ migrations/0008-agency-client-projection-create-index.js
@@ -11,22 +11,15 @@ const indexOptions = {
 module.exports = {
   id: '0008-agency-client-projection-create-index',
   up: (db, cb) => {
-    db.createCollection(collectionName, {collation: {locale: 'en_US', strength: 2}}, (err) => {
-      //if collection already exists, it does not throw error
-      if (err) {
-        return cb(err);
-      }
-      ensureIndexExists(
-        db.collection(collectionName),
-        indexDetails,
-        indexOptions
-      ).then(() => {
-        cb();
-      }).catch((err) => {
-        cb(err);
-      });
+    ensureIndexExists(
+      db.collection(collectionName),
+      indexDetails,
+      indexOptions
+    ).then(() => {
+      cb();
+    }).catch((err) => {
+      cb(err);
     });
-
   },
 
   down: (db, cb) => {
