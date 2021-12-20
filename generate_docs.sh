74f6d7ba2db849c6815022e5803ee29d1012b0a5
Michael Barnard <michael.barnard@a24group.com>
2021-12-20 13:37:50 +0200

    Attempt at a regenerations gcloud
    Attempt at a regenerations gcloud

---
+++ generate_docs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+# Get the last commit author email
+LAST_COMMIT_BY=$(git log -1 --pretty=%ae)
+
+# Only run if the commit was not made by ci user
+if [ "$LAST_COMMIT_BY" == "devonly@a24group.com" ]; then
+    echo "Commit made by ci user, no further actions"
+else
+    # Get the git branch that we are on
+    GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    echo "Branch identified as $GIT_BRANCH"
+
+    # Generate the docs
+    npm run dev-generate-event-docs
+
+    # Check if the Events.md was changed
+    REGENERATED=$( git status -s | grep "Events.md" )
+
+    if [ -z "$REGENERATED" ]; then
+        echo "Events.md had no changes" # No Changes
+    else
+        echo "Events.md changed, committing...." 
+
+        echo "Setting git globals..."
+        git config --global user.name "CI"
+        git config --global user.email "devonly@a24group.com"
+        echo "Adding files..."
+        git add Events.md
+        echo "Comitting..."
+        git commit -m 'Update event documentation'
+        echo "Pushing..."
+        git push origin $GIT_BRANCH
+        echo "Pushed"
+    fi
+fi
