ecb34188fcd15d082d194a78b985fe911ec2e688
NeilNienaber <neil.nienaber@gmail.com>
2021-12-28 10:02:47 +0200

    Adding some chaos monkey events to the consumption code
    Adding some chaos monkey events to the consumption code

--- src/event-store-projection/consumer/EventStoreProjectionProcessor.ts
+++ src/event-store-projection/consumer/EventStoreProjectionProcessor.ts
@@ -3,9 +3,19 @@ import {random, toNumber} from 'lodash';
 import {PubSubMessageMetaDataInterface} from 'PubSubMessageMetaDataInterface';
 import {EventStoreModelInterface} from '../../models/EventStore';
 import {EventStoreProjection} from '../../models/EventStoreProjection';
+const NACK = 'NACK';
+const TIMEOUT = 'TIMEOUT';
+const CRASH = 'CRASH';
+
+const SUBSCRIBER_TIMEOUT = 10000;
 const CONSUMER_NAME = process.argv[2];
 const MIN_DELAY = toNumber(process.argv[3]);
 const MAX_DELAY = toNumber(process.argv[4]);
+const CHAOS_CODES = process.argv[5].split(',');
+const DELAY = random(MIN_DELAY, MAX_DELAY);
+const MAX_MESSAGES = random(10, 100);
+
+let handledMessages = 0;
 
 interface MessageInterface {
   event: EventStoreModelInterface;
@@ -21,9 +31,30 @@ module.exports = async (
   logger.debug('event received', message);
   const receivedAt = new Date();
   const publishedAt = new Date(message.published_at);
-  const delay = random(MIN_DELAY, MAX_DELAY);
 
+  handledMessages++;
   setTimeout(() => {
+    if (handledMessages > MAX_MESSAGES && CHAOS_CODES.includes(CRASH)) {
+      logger.info('CHAOS_CODE::CRASH invoked for event', {consumer: CONSUMER_NAME, event_id: message.event._id.toString()});
+      process.exit(1);
+    }
+
+    if (DELAY % 9 == 0 && CHAOS_CODES.includes(NACK)) {
+      const err = new Error(`CHAOS_CODE::NACK invoked for event ${message.event._id.toString()}`);
+
+      logger.info('CHAOS::NACK invoked for event', {consumer: CONSUMER_NAME, original_error: err, event_id: message.event._id.toString()});
+      return callback(err);
+    }
+
+    if (DELAY % 8 == 0 && CHAOS_CODES.includes(TIMEOUT)) {
+      logger.info('CHAOS_CODE::TIMEOUT invoked for event', {consumer: CONSUMER_NAME, event_id: message.event._id.toString()});
+      setTimeout(() => {
+        logger.info('CHAOS_CODE::TIMEOUT executed for event', {consumer: CONSUMER_NAME, event_id: message.event._id.toString()});
+        return callback();
+      }, SUBSCRIBER_TIMEOUT);
+      return;
+    }
+
     const consumedAt = new Date();
 
     EventStoreProjection.create(
@@ -44,5 +75,5 @@ module.exports = async (
         return callback();
       }
     );
-  }, delay);
+  }, DELAY);
 };
