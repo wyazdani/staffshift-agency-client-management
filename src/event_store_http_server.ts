f369beb755ba781b19db56be5ac507d9572c4e38
Mehran Khajavi <sinoohe.mkh@gmail.com>
2022-02-04 18:54:23 +0330

    EOD: implement projections and seed #129

---
+++ src/event_store_http_server.ts
@@ -0,0 +1,36 @@
+import Logger from 'a24-logzio-winston';
+import config from 'config';
+import {GracefulShutdownConfigurationInterface} from 'GracefulShutdownConfigurationInterface';
+import {EventStoreHttpServer} from 'ss-eventstore';
+
+Logger.setup(config.get('logger'));
+const loggerContext = Logger.getContext('event-store-http-server');
+const eventStoreHttpServer = new EventStoreHttpServer({
+  apiToken: config.get<string>('event_store.http_server.api_token'),
+  mongo: {
+    database_host: config.get<string>('event_store.mongo.database_host'),
+    options: config.get('event_store.mongo.options')
+  },
+  server: {port: config.get('event_store.http_server.port')}
+});
+
+const shutdown = async () => {
+  try {
+    loggerContext.info('Starting to shutdown');
+    await eventStoreHttpServer.shutdown();
+    loggerContext.info('Shutdown done');
+    process.exit(0);
+  } catch (error) {
+    loggerContext.error('Error while shutting down event store http server', error);
+    process.exit(1);
+  }
+}
+
+for (const signal of config.get<GracefulShutdownConfigurationInterface>('graceful_shutdown').signals) {
+  process.on(signal, () => shutdown());
+}
+(async () => {
+  loggerContext.info('Starting event store http server');
+  await eventStoreHttpServer.start();
+  loggerContext.info('http server started');
+})();
---
+++ src/event_store_publisher.ts
@@ -0,0 +1,49 @@
+import Logger from 'a24-logzio-winston';
+import config from 'config';
+import {GracefulShutdownConfigurationInterface} from 'GracefulShutdownConfigurationInterface';
+import {EventStorePublisherConfigInterface, EventStorePublisher} from 'ss-eventstore';
+import {setTimeout} from 'timers/promises';
+
+Logger.setup(config.get('logger'));
+const loggerContext = Logger.getContext('event-store-publisher');
+
+const eventStorePublisher = new EventStorePublisher({
+  messagePublisher: {
+    api_end_point: config.get('event_store.pub_sub_api_end_point'),
+    enable_message_ordering: true,
+    env: process.env.NODE_ENV || 'development',
+    auth: config.get('pubSubAuth')
+  },
+  mongo: {
+    database_host: config.get('event_store.mongo.database_host'),
+    options: config.get('event_store.mongo.options')
+  },
+  publishTopicName: config.get<string>('event_store.topic_name'),
+  pipelineId: config.get<string>('event_store.pipeline_id')
+} as EventStorePublisherConfigInterface);
+
+const shutdown = async (success: boolean) => {
+  loggerContext.info('Calling shutdown on event store publisher');
+  try {
+    await eventStorePublisher.shutdown();
+    process.exit(success ? 0 : 1);
+  } catch (error) {
+    loggerContext.error('Error exiting event store publisher', error);
+    await setTimeout(200);
+    process.exit(1);
+  }
+};
+
+eventStorePublisher.on('error', (error) => {
+  loggerContext.error('Error in Event Store Publisher', {error});
+  shutdown(false);
+});
+
+for (const signal of config.get<GracefulShutdownConfigurationInterface>('graceful_shutdown').signals) {
+  process.on(signal, () => shutdown(true));
+}
+(async () => {
+  loggerContext.info('starting publisher');
+  await eventStorePublisher.start();
+  loggerContext.info('publisher started');
+})();
