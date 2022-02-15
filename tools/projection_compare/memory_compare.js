28ceb7e088d369926f2053020d1a11d32788410f
NeilNienaber <neil.nienaber@gmail.com>
2022-02-15 11:15:21 +0200

    Minor updates
    Improved logs
    Added TODO
    Added additional comparison check

--- tools/projection_compare/memory_compare.js
+++ tools/projection_compare/memory_compare.js
@@ -3,24 +3,30 @@ const MongoClient = require('mongodb').MongoClient;
 const config = require('config');
 const _ = require('lodash');
 const comparisonFields = {
-  'source': ['linked', 'client_type', "created_at"],
-  'destination': ['linked', 'client_type'],
+  'source': ['agency_id', 'name', 'description', 'max_consultants', 'status'],
+  'destination': ['agency_id', 'name', 'description', 'max_consultants', 'status'],
 }
 
 const run = async () => {
+  console.log('Comparison script started');
   const client = await MongoClient.connect(config.mongo.connection, { useUnifiedTopology: true });
   const db = client.db(config.mongo.database);
   const SourceCollection = db.collection(config.mongo.source_collection);
   const DestinationCollection = db.collection(config.mongo.destination_collection);
   const sourceData = await SourceCollection.find({}).toArray();
-  console.log('Source data retrieved');
+  console.log(`Source data retrieved from collection: ${config.mongo.source_collection}`);
   const destinationData = await DestinationCollection.find({}).toArray();
-  console.log('Destination data retrieved');
+  console.log(`Destination data retrieved from collection: ${config.mongo.destination_collection}`);
+
+  if (sourceData.length !== destinationData.length) {
+    console.error(`Source length: ${sourceData.length} and Destination length: ${destinationData.length} do not match`);
+  }
 
   let counter = 0;
   for (const item of sourceData) {
     counter++;
-    const destItem =  _.find(destinationData, {agency_id: item.agency_id, client_id: item.client_id});
+    // TODO improve find to remove need to always alter the code.
+    const destItem =  _.find(destinationData, {_id: item._id});
     if (!destItem) {
       console.error('DESTINATION MISSING DOCUMENT', item);
     }
