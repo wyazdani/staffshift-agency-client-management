07ec4f6d7b29fbc458dac0a0ff4ca6d27d33528b
Mehran Khajavi <sinoohe.mkh@gmail.com>
2022-02-17 16:13:16 +0330

    Remove writing to old collections #143

--- src/streaming_applications/AgencyClientConsultantProjection/watch.ts
+++
@@ -1,7 +0,0 @@
-import {AgencyClientConsultantProjectionCorePipeline} from './pipelines/AgencyClientConsultantProjectionCorePipeline';
-import {Watcher, WatcherContext} from '../core/Watcher';
-
-const pipelines = [new AgencyClientConsultantProjectionCorePipeline()];
-const watcherContext: WatcherContext = Watcher.getWatcherContext('AgencyClientConsultant', pipelines);
-
-export default watcherContext;
--- src/streaming_applications/AgencyConsultantRolesProjection/watch.ts
+++
@@ -1,7 +0,0 @@
-import {AgencyConsultantProjectionPipeline} from './AgencyConsultantProjectionPipeline';
-import {Watcher, WatcherContext} from '../core/Watcher';
-
-const pipelines = [new AgencyConsultantProjectionPipeline()];
-const watcherContext: WatcherContext = Watcher.getWatcherContext('AgencyClientEventLogProjection', pipelines);
-
-export default watcherContext;
