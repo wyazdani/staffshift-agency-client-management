16c77d9e8b3ba1eb5faf3136863ceb846d139ccc
Mehran Khajavi <sinoohe.mkh@gmail.com>
2022-03-24 14:36:05 +0430

    Write test cases for command handler for consultant job assign #157

--- tests/aggregates/ConsultantJobAssign/command-handlers/CompleteConsultantJobAssignCommandHandler.spec.ts
+++ tests/aggregates/ConsultantJobAssign/command-handlers/CompleteConsultantJobAssignCommandHandler.spec.ts
@@ -1,10 +1,10 @@
 import sinon, {stubInterface} from 'ts-sinon';
-import {StartConsultantJobAssignCommandHandler} from '../../../../src/aggregates/ConsultantJobAssign/command-handlers/StartConsultantJobAssignCommandHandler';
+import {CompleteConsultantJobAssignCommandHandler} from '../../../../src/aggregates/ConsultantJobAssign/command-handlers/CompleteConsultantJobAssignCommandHandler';
 import {ConsultantJobAssignAggregate} from '../../../../src/aggregates/ConsultantJobAssign/ConsultantJobAssignAggregate';
 import {ConsultantJobAssignRepository} from '../../../../src/aggregates/ConsultantJobAssign/ConsultantJobAssignRepository';
 import {EventsEnum} from '../../../../src/Events';
 
-describe('StartConsultantAssignCommandHandler class', () => {
+describe('CompleteConsultantAssignCommandHandler class', () => {
   const agencyId = 'agency id';
   const jobId = 'job id';
 
@@ -17,7 +17,7 @@ describe('StartConsultantAssignCommandHandler class', () => {
       const aggregateId: any = {sample: 'ok'};
       const repository = stubInterface<ConsultantJobAssignRepository>();
       const aggregate = stubInterface<ConsultantJobAssignAggregate>();
-      const handler = new StartConsultantJobAssignCommandHandler(repository);
+      const handler = new CompleteConsultantJobAssignCommandHandler(repository);
 
       repository.getAggregate.resolves(aggregate);
       aggregate.getLastEventId.returns(eventId);
@@ -26,7 +26,7 @@ describe('StartConsultantAssignCommandHandler class', () => {
       await handler.execute(agencyId, jobId, {});
       repository.save.should.have.been.calledWith([
         {
-          type: EventsEnum.CONSULTANT_JOB_ASSIGN_PROCESS_STARTED,
+          type: EventsEnum.CONSULTANT_JOB_ASSIGN_PROCESS_COMPLETED,
           aggregate_id: aggregate.getId(),
           data: {},
           sequence_id: 3
--- tests/aggregates/ConsultantJobAssign/command-handlers/SucceedItemConsultantJobAssignCommandHandler.spec.ts
+++ tests/aggregates/ConsultantJobAssign/command-handlers/SucceedItemConsultantJobAssignCommandHandler.spec.ts
@@ -1,10 +1,10 @@
 import sinon, {stubInterface} from 'ts-sinon';
-import {StartConsultantJobAssignCommandHandler} from '../../../../src/aggregates/ConsultantJobAssign/command-handlers/StartConsultantJobAssignCommandHandler';
+import {SucceedItemConsultantJobAssignCommandHandler} from '../../../../src/aggregates/ConsultantJobAssign/command-handlers/SucceedItemConsultantJobAssignCommandHandler';
 import {ConsultantJobAssignAggregate} from '../../../../src/aggregates/ConsultantJobAssign/ConsultantJobAssignAggregate';
 import {ConsultantJobAssignRepository} from '../../../../src/aggregates/ConsultantJobAssign/ConsultantJobAssignRepository';
 import {EventsEnum} from '../../../../src/Events';
 
-describe('StartConsultantAssignCommandHandler class', () => {
+describe('SucceedItemConsultantAssignCommandHandler class', () => {
   const agencyId = 'agency id';
   const jobId = 'job id';
 
@@ -15,20 +15,23 @@ describe('StartConsultantAssignCommandHandler class', () => {
     it('Test running the command', async () => {
       const eventId = 2;
       const aggregateId: any = {sample: 'ok'};
+      const commandData: any = {
+        client_id: 'client id'
+      };
       const repository = stubInterface<ConsultantJobAssignRepository>();
       const aggregate = stubInterface<ConsultantJobAssignAggregate>();
-      const handler = new StartConsultantJobAssignCommandHandler(repository);
+      const handler = new SucceedItemConsultantJobAssignCommandHandler(repository);
 
       repository.getAggregate.resolves(aggregate);
       aggregate.getLastEventId.returns(eventId);
       aggregate.getId.returns(aggregateId);
       repository.save.resolves();
-      await handler.execute(agencyId, jobId, {});
+      await handler.execute(agencyId, jobId, commandData);
       repository.save.should.have.been.calledWith([
         {
-          type: EventsEnum.CONSULTANT_JOB_ASSIGN_PROCESS_STARTED,
+          type: EventsEnum.CONSULTANT_JOB_ASSIGN_PROCESS_ITEM_SUCCEEDED,
           aggregate_id: aggregate.getId(),
-          data: {},
+          data: commandData,
           sequence_id: 3
         }
       ]);
