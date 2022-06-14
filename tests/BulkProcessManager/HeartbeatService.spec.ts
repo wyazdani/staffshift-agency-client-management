626e028e08f0bdc6a2cace6ef542193662895e8b
Mehran Khajavi <sinoohe.mkh@gmail.com>
2022-06-14 18:25:08 +0430

    hotfix random failing test case #180

--- tests/BulkProcessManager/HeartbeatService.spec.ts
+++ tests/BulkProcessManager/HeartbeatService.spec.ts
@@ -1,49 +1,59 @@
-import * as mockdate from 'mockdate';
 import sinon from 'ts-sinon';
 import {HeartbeatService} from '../../src/BulkProcessManager/HeartbeatService';
 import {BulkProcessManagerV1} from '../../src/models/BulkProcessManagerV1';
 import {TestUtilsLogger} from '../tools/TestUtilsLogger';
-import {setTimeout} from 'timers/promises';
 
 describe('HeartbeatService', () => {
   afterEach(() => {
     sinon.restore();
-    mockdate.reset();
   });
   describe('start() and stop()', () => {
     it('Test updating heartbeat', async () => {
       const date = new Date();
-
-      mockdate.set(date);
+      const clock = sinon.useFakeTimers(date);
       const logger = TestUtilsLogger.getLogger(sinon.spy());
       const processId = 'process id';
       const updateOne = sinon.stub(BulkProcessManagerV1, 'updateOne').resolves();
-      const heartbeat = new HeartbeatService(logger, processId, 50);
+      const heartbeat = new HeartbeatService(logger, processId, 5000);
 
       heartbeat.start();
-      await setTimeout(120);
+      await clock.tickAsync(12000);
+
       heartbeat.stop();
       updateOne.should.have.been.called.calledTwice;
-      updateOne.should.have.been.calledWith(
+      updateOne.firstCall.should.have.been.calledWith(
         {
           _id: processId
         },
         {
           $set: {
-            heart_beat: date
+            heart_beat: new Date(date.getTime() + 5000)
+          }
+        }
+      );
+      updateOne.secondCall.should.have.been.calledWith(
+        {
+          _id: processId
+        },
+        {
+          $set: {
+            heart_beat: new Date(date.getTime() + 10000)
           }
         }
       );
     });
 
     it('Test updating heartbeat fails does not throw error', async () => {
+      const date = new Date();
+      const clock = sinon.useFakeTimers(date);
       const logger = TestUtilsLogger.getLogger(sinon.spy());
       const processId = 'process id';
       const updateOne = sinon.stub(BulkProcessManagerV1, 'updateOne').rejects(new Error('sample'));
-      const heartbeat = new HeartbeatService(logger, processId, 50);
+      const heartbeat = new HeartbeatService(logger, processId, 5000);
 
       heartbeat.start();
-      await setTimeout(60);
+      await clock.tickAsync(5010);
+
       heartbeat.stop();
       updateOne.should.have.been.called.calledOnce;
     });
