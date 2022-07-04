797bbc523038b242e7cbdeba5afde92181f24d27
Waqar Yazdani <waqaryazdani2@gmail.com>
2022-07-04 19:01:56 +0500

    Added Test Cases

--- tests/projections/AgencyClientsV2/Projector.spec.ts
+++ tests/projections/AgencyClientsV2/Projector.spec.ts
@@ -1,6 +1,7 @@
 import {LoggerContext} from 'a24-logzio-winston';
 import sinon from 'sinon';
-import {stubConstructor} from 'ts-sinon';
+import {EventStorePubSubModelInterface} from 'ss-eventstore/dist/declarations';
+import {stubConstructor, stubInterface} from 'ts-sinon';
 import {EventsEnum} from '../../../src/Events';
 import {AgencyClientsProjectionV2} from '../../../src/models/AgencyClientsProjectionV2';
 import AgencyClientsProjector from '../../../src/projections/AgencyClientsV2/AgencyClientsProjector';
@@ -8,6 +9,7 @@ import {AgencyClientLinkedEventHandler} from '../../../src/projections/AgencyCli
 import {AgencyClientSyncedEventHandler} from '../../../src/projections/AgencyClientsV2/event-handlers/AgencyClientSyncedEventHandler';
 import {AgencyClientUnLinkedEventHandler} from '../../../src/projections/AgencyClientsV2/event-handlers/AgencyClientUnLinkedEventHandler';
 import {EventHandlerFactory} from '../../../src/projections/AgencyClientsV2/factories/EventHandlerFactory';
+import {EventHandlerInterface} from '../../../src/types/EventHandlerInterface';
 import {TestUtilsLogger} from '../../tools/TestUtilsLogger';
 
 describe('projections/AgencyClients/Projector', () => {
@@ -19,9 +21,32 @@ describe('projections/AgencyClients/Projector', () => {
   });
   afterEach(() => sinon.restore());
   describe('project()', async () => {
+    const events = [
+      EventsEnum.AGENCY_CLIENT_LINKED,
+      EventsEnum.AGENCY_CLIENT_UNLINKED,
+      EventsEnum.AGENCY_CLIENT_SYNCED
+    ];
     const agencyId = '60126eb559f35a4f3c34ff07';
     const clientId = '60126eb559f35a4f3c34ff06';
     const orgId = '61b8991abfb74a7157c6d88f';
+
+    for (const eventType of events) {
+      it(`Test ${eventType}`, async () => {
+        const event: any = {
+          type: eventType
+        };
+        const handler = stubInterface<EventHandlerInterface<EventStorePubSubModelInterface>>();
+
+        handler.handle.resolves();
+        const getHandler = sinon.stub(EventHandlerFactory, 'getHandler').returns(handler);
+        const logger = TestUtilsLogger.getLogger(sinon.spy());
+        const projector = new AgencyClientsProjector();
+
+        await projector.project(logger, event);
+        getHandler.should.have.been.calledWith(event.type, logger);
+        handler.handle.should.have.been.calledWith(event);
+      });
+    }
 
     it('test that unsupported event are consumed successfully', async () => {
       const event: any = {
