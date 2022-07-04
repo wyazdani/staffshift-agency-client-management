82b641adff79bc95d1834700f245c505dd6b26fa
Waqar Yazdani <waqaryazdani2@gmail.com>
2022-07-04 18:55:37 +0500

    Test Cases Updated 198

--- tests/projections/AgencyConsultantRolesV2/AgencyConsultantRolesProjector.spec.ts
+++ tests/projections/AgencyConsultantRolesV2/AgencyConsultantRolesProjector.spec.ts
@@ -1,4 +1,4 @@
-import sinon, {stubConstructor} from 'ts-sinon';
+import sinon, {stubConstructor, stubInterface} from 'ts-sinon';
 import {EventsEnum} from '../../../src/Events';
 import {AgencyConsultantRolesProjectionV2} from '../../../src/models/AgencyConsultantRolesProjectionV2';
 import AgencyConsultantRolesProjector from '../../../src/projections/AgencyConsultantRolesV2/AgencyConsultantRolesProjector';
@@ -9,9 +9,17 @@ import {AgencyConsultantRoleEnabledEventHandler} from '../../../src/projections/
 import {AgencyConsultantRoleDisabledEventHandler} from '../../../src/projections/AgencyConsultantRolesV2/event-handlers/AgencyConsultantRoleDisabledEventHandler';
 import {AgencyConsultantRoleDetailsUpdatedEventHandler} from '../../../src/projections/AgencyConsultantRolesV2/event-handlers/AgencyConsultantRoleDetailsUpdatedEventHandler';
 import {MONGO_ERROR_CODES} from 'staffshift-node-enums';
+import {EventStorePubSubModelInterface} from 'ss-eventstore/dist/declarations';
+import {EventHandlerInterface} from '../../../src/types/EventHandlerInterface';
 
 describe('AgencyConsultantRolesProjectorV2', () => {
   describe('project', () => {
+    const events = [
+      EventsEnum.AGENCY_CONSULTANT_ROLE_ADDED,
+      EventsEnum.AGENCY_CONSULTANT_ROLE_ENABLED,
+      EventsEnum.AGENCY_CONSULTANT_ROLE_DISABLED,
+      EventsEnum.AGENCY_CONSULTANT_ROLE_DETAILS_UPDATED
+    ];
     const agencyId = '60126eb559f35a4f3c34ff07';
     const consultantRoleId = '60126eb559f35a4f3c34ff06';
 
@@ -30,6 +38,24 @@ describe('AgencyConsultantRolesProjectorV2', () => {
       await projector.project(TestUtilsLogger.getLogger(sinon.spy()), event);
       handlerStub.handle.should.not.have.been.called;
     });
+
+    for (const eventType of events) {
+      it(`Test ${eventType}`, async () => {
+        const event: any = {
+          type: eventType
+        };
+        const handler = stubInterface<EventHandlerInterface<EventStorePubSubModelInterface>>();
+
+        handler.handle.resolves();
+        const getHandler = sinon.stub(EventHandlerFactory, 'getHandler').returns(handler);
+        const logger = TestUtilsLogger.getLogger(sinon.spy());
+        const projector = new AgencyConsultantRolesProjector();
+
+        await projector.project(logger, event);
+        getHandler.should.have.been.calledWith(event.type, logger);
+        handler.handle.should.have.been.calledWith(event);
+      });
+    }
 
     it('Test AGENCY_CONSULTANT_ROLE_ADDED', async () => {
       const event: any = {
