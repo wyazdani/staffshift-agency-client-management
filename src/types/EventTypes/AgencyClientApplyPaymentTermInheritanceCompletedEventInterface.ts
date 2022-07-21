b28d983a0509930c87d181d885a30820d0409761
Waqar Yazdani <waqaryazdani2@gmail.com>
2022-07-21 17:59:35 +0500

    Renaming EventTypes

---
+++ src/types/EventTypes/AgencyClientApplyPaymentTermInheritanceCompletedEventInterface.ts
@@ -0,0 +1,14 @@
+import {BaseEventStoreDataInterface} from 'EventTypes';
+import {EventStorePubSubModelInterface} from 'ss-eventstore';
+import {OrganisationJobAggregateIdInterface} from '../../aggregates/OrganisationJob/types';
+
+export interface AgencyClientApplyPaymentTermInheritanceCompletedDataInterface extends BaseEventStoreDataInterface {
+  _id: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface AgencyClientApplyPaymentTermInheritanceCompletedEventInterface
+  extends EventStorePubSubModelInterface<
+    AgencyClientApplyPaymentTermInheritanceCompletedDataInterface,
+    OrganisationJobAggregateIdInterface
+  > {}
---
+++ src/types/EventTypes/AgencyClientApplyPaymentTermInitiatedEventInterface.ts
@@ -0,0 +1,16 @@
+import {BaseEventStoreDataInterface} from 'EventTypes';
+import {OrganisationJobAggregateIdInterface} from '../../aggregates/OrganisationJob/types';
+import {EventStorePubSubModelInterface} from 'ss-eventstore';
+
+export interface AgencyClientApplyPaymentTermInitiatedDataInterface extends BaseEventStoreDataInterface {
+  _id: string;
+  term: string;
+  client_id: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface AgencyClientApplyPaymentTermInitiatedEventInterface
+  extends EventStorePubSubModelInterface<
+    AgencyClientApplyPaymentTermInitiatedDataInterface,
+    OrganisationJobAggregateIdInterface
+  > {}
