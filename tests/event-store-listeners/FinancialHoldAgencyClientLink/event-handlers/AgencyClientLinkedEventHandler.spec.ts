16e23ec68c56e622f9295b2ccc616976fb7c5793
Waqar Yazdani <waqaryazdani2@gmail.com>
2022-08-23 12:58:04 +0500

    Test Cases #233

---
+++ tests/event-store-listeners/FinancialHoldAgencyClientLink/event-handlers/AgencyClientLinkedEventHandler.spec.ts
@@ -0,0 +1,30 @@
+import sinon, {stubInterface} from 'ts-sinon';
+import {AgencyClientLinkedEventHandler} from '../../../../src/event-store-listeners/FinancialHoldAgencyClientLink/event-handlers/AgencyClientLinkedEventHandler';
+import {FinancialHoldAgencyClientLinkPropagator} from '../../../../src/event-store-listeners/FinancialHoldAgencyClientLink/event-handlers/FinancialHoldAgencyClientLinkPropagator';
+import {EventRepository} from '../../../../src/EventRepository';
+import {TestUtilsLogger} from '../../../tools/TestUtilsLogger';
+
+describe('AgencyClientLinkedEventHandler', () => {
+  describe('handle()', async () => {
+    afterEach(() => sinon.restore());
+    it('Test calling propagate', async () => {
+      const event: any = {
+        aggregate_id: {
+          name: 'the name'
+        },
+        type: 'type sample',
+        data: {
+          ok: 'oops'
+        }
+      };
+      const propagate = sinon.stub(FinancialHoldAgencyClientLinkPropagator.prototype, 'propagate').resolves();
+      const handler = new AgencyClientLinkedEventHandler(
+        TestUtilsLogger.getLogger(sinon.spy()),
+        stubInterface<EventRepository>()
+      );
+
+      await handler.handle(event);
+      propagate.should.have.been.calledOnceWith(event.aggregate_id, event.data);
+    });
+  });
+});
