f95e30e2461e986e2775d3247e92cd355cd3028a
RO-Owl <anthonystachel.owl@gmail.com>
2022-08-24 14:31:29 +0200

    updated test cases and changed agencyid and organisation id to get added to options regardless #245

--- src/helpers/FacadeClientHelper.ts
+++ src/helpers/FacadeClientHelper.ts
@@ -45,12 +45,12 @@ export class FacadeClientHelper {
   ): Promise<AgencyOrganisationLinkDataType[]> {
     if (!options) {
       options = {
-        xRequestId: this.logger.requestId,
-        agencyId: agencyId,
-        organisationId: organisationId,
-        agencyOrgType: 'organisation'
+        xRequestId: this.logger.requestId
       };
     }
+    options.agencyOrgType = 'organisation';
+    options.agencyId = agencyId;
+    options.organisationId = organisationId;
     //if options isn't defined, it won't create agencyId,agencyOrgType and organisationId properties #https://github.com/A24Group/staffshift-agency-client-management/issues/245
 
     if (siteId) {
