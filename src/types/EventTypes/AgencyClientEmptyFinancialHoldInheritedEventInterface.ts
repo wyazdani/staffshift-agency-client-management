2a7dd7bd4326ee3ca5d23175a72767de6c44d641
Waqar Yazdani <waqaryazdani2@gmail.com>
2022-08-24 15:06:17 +0500

    Note Property #247

--- src/types/EventTypes/AgencyClientEmptyFinancialHoldInheritedEventInterface.ts
+++ src/types/EventTypes/AgencyClientEmptyFinancialHoldInheritedEventInterface.ts
@@ -3,7 +3,9 @@ import {EventStorePubSubModelInterface} from 'ss-eventstore';
 import {FinancialHoldAggregateIdInterface} from '../../aggregates/FinancialHold/types';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface AgencyClientEmptyFinancialHoldInheritedEventStoreDataInterface extends BaseEventStoreDataInterface {}
+export interface AgencyClientEmptyFinancialHoldInheritedEventStoreDataInterface extends BaseEventStoreDataInterface {
+  note: string;
+}
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface AgencyClientEmptyFinancialHoldInheritedEventInterface
