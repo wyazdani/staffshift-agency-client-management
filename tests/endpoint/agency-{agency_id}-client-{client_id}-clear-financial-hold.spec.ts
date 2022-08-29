16d6a92b32511a4cb4eb43e3735d3604958caed6
Waqar Yazdani <waqaryazdani2@gmail.com>
2022-08-29 12:50:41 +0500

    Clear/Apply Financial Change #253

--- tests/endpoint/agency-{agency_id}-client-{client_id}-clear-financial-hold.spec.ts
+++ tests/endpoint/agency-{agency_id}-client-{client_id}-clear-financial-hold.spec.ts
@@ -33,78 +33,70 @@ describe('agency-{agency_id}-client-{client_id}-financial-hold', () => {
     await organisationJobScenario.deleteAllEvents();
   });
 
-  describe('get', () => {
-    const agencyId = '62dfd97a38ca805fd2000001';
-    const clientId = '62dfd98402f815105b000001';
-    const jwtToken = getJWT({
-      sub: '5ff6e098fb83732f8e23dc92',
-      name: 'John Doe',
-      iat: 1516239022
-    });
-    const headers = {
-      'x-request-jwt': jwtToken,
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      'X-Request-Id': '123'
-    };
+  describe('post', () => {
+    const agencyId = '6141d5be5863dc2202000001';
+    const clientId = '6141d64365e0e52381000001';
 
-    afterEach(async () => {
-      await AgencyClientFinancialHoldsProjectionScenarios.removeAll();
+    it('should respond with 202 clears financial hold', async () => {
+      await AgencyClientsProjectionScenarios.createRecord({
+        agency_id: agencyId
+      });
+      const res = await api
+        .post(`/agency/${agencyId}/client/${clientId}/clear-financial-hold`)
+        .set(headers)
+        .send(payload);
+
+      res.statusCode.should.equal(202);
     });
-    it('should respond with 200 Retrieves a single Agency Client financial hold', async () => {
+
+    it('should respond with 400 Validation Error', async () => {
       const schema = {
         type: 'object',
-        required: ['_id', 'agency_id', 'client_id', 'inherited', 'financial_hold', 'updated_at', 'created_at', '__v'],
+        required: ['code', 'message'],
         properties: {
-          _id: {
+          code: {
             type: 'string',
-            pattern: '^[0-9a-fA-F]{24}$'
+            enum: ['REQUIRED', 'PATTERN', 'SCHEMA_VALIDATION_FAILED', 'MODEL_VALIDATION_FAILED']
           },
-          agency_id: {
-            type: 'string',
-            pattern: '^[0-9a-fA-F]{24}$'
-          },
-          client_id: {
-            type: 'string',
-            pattern: '^[0-9a-fA-F]{24}$'
-          },
-          inherited: {
-            type: 'boolean'
-          },
-          financial_hold: {
-            type: 'string',
-            enum: ['applied', 'cleared', 'not_set']
-          },
-          note: {
+          message: {
             type: 'string'
           },
-          updated_at: {
-            type: 'string',
-            format: 'date-time'
-          },
-          created_at: {
-            type: 'string',
-            format: 'date-time'
-          },
-          __v: {
-            type: 'integer'
+          errors: {
+            type: 'array',
+            items: {
+              type: 'object',
+              required: ['code', 'message', 'path'],
+              properties: {
+                code: {
+                  type: 'string',
+                  enum: ['OBJECT_ADDITIONAL_PROPERTIES', 'OBJECT_MISSING_REQUIRED_PROPERTY']
+                },
+                message: {
+                  type: 'string'
+                },
+                path: {
+                  type: 'array',
+                  items: {
+                    type: 'string'
+                  }
+                },
+                description: {
+                  type: 'string'
+                }
+              },
+              additionalProperties: false
+            }
           }
         },
         additionalProperties: false
       };
+      const res = await api.post(`/agency/${agencyId}/client/${clientId}/clear-financial-hold`).set(headers).send();
 
-      await AgencyClientFinancialHoldsProjectionScenarios.create({
-        agency_id: agencyId,
-        client_id: clientId
-      });
-
-      const res = await api.get(`/agency/${agencyId}/client/${clientId}/financial-hold`).set(headers).send();
-
-      assert.equal(res.statusCode, 200);
+      assert.equal(res.statusCode, 400);
       assert.isTrue(validator.validate(res.body, schema), 'response schema expected to be valid');
     });
 
-    it('should respond with 401', async () => {
+    it('should respond with 401 Failed to authenticate', async () => {
       const schema = {
         type: 'object',
         required: ['code', 'message'],
@@ -122,7 +114,10 @@ describe('agency-{agency_id}-client-{client_id}-financial-hold', () => {
       const otherHeaders = cloneDeep(headers);
 
       otherHeaders['x-request-jwt'] = 'invalid';
-      const res = await api.get(`/agency/${agencyId}/client/${clientId}/financial-hold`).set(otherHeaders).send();
+      const res = await api
+        .post(`/agency/${agencyId}/client/${clientId}/clear-financial-hold`)
+        .set(otherHeaders)
+        .send(payload);
 
       assert.equal(res.statusCode, 401);
       assert.isTrue(validator.validate(res.body, schema), 'response schema expected to be valid');
@@ -145,9 +140,12 @@ describe('agency-{agency_id}-client-{client_id}-financial-hold', () => {
         additionalProperties: false
       };
 
-      const res = await api.get(`/agency/${agencyId}/consultant-roles/${clientId}`).set(headers).send();
+      const res = await api
+        .post(`/agency/${agencyId}/client/${clientId}/clear-financial-hold`)
+        .set(headers)
+        .send(payload);
 
-      assert.equal(res.statusCode, 404);
+      res.statusCode.should.equal(404);
       assert.isTrue(validator.validate(res.body, schema), 'response schema expected to be valid');
     });
   });
