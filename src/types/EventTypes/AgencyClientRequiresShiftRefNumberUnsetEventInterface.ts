abae608b9863329e22c53189271867556a8fc842
RO-Owl <anthonystachel.owl@gmail.com>
2022-09-05 10:43:17 +0200

    pr fixes #266

--- src/types/EventTypes/AgencyClientRequiresShiftRefNumberUnsetEventInterface.ts
+++ src/types/EventTypes/AgencyClientRequiresShiftRefNumberUnsetEventInterface.ts
@@ -1,7 +1,7 @@
 import {BaseEventStoreDataInterface} from 'EventTypes';
 import {BookingPreferenceAggregateIdInterface} from '../../aggregates/BookingPreference/types';
 import {EventStorePubSubModelInterface} from 'ss-eventstore';
-
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface AgencyClientRequiresShiftRefNumberUnsetEventStoreDataInterface extends BaseEventStoreDataInterface {}
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
